6b67dde518fbd30726d1e34a04435030ef1eb21e
Joseph Colicchio <jcolicchio@novacoast.com>
2015-02-25 20:54:46 -0800

    Document the example servers

--- custom/ChatServer.js
+++ custom/ChatServer.js
@@ -1,14 +1,25 @@
+// TODO: consider moving "Type" to the first arg?
+// TODO: consider an object with keys as a single argument
+
+// require Server.js with default name, Chat type, default other properties
 var Server = require('../Server.js')(null, "Chat");
 
-var Player = require('../html/game/Player.js');
+// Player is the object being sent back and forth
+var Player = require('../html/chat/Player.js');
 
-var players = [];
+// iterate playerId as new players are created
+// i suppose this could go in the Player class/constructor?
 var playerId = 0;
 
+// maintain a chat history:
+// {name: who sent it, message: the message text}
 var chatHistory = [];
+
+// maintain a list of most recent 10 messages
 var chatHistoryLength = 10;
 
 var pushChatHistory = function(entry) {
+	// push an arbitrary entry to the chat history, and remove oldest ones from queue if need be
 	chatHistory.push(entry);
 	if(chatHistory.length > chatHistoryLength) {
 		chatHistory.splice(0, chatHistory.length - chatHistoryLength);
@@ -16,18 +27,33 @@ var pushChatHistory = function(entry) {
 }
 
 Server.onConnect = function(client) {
+	// this callback occurs when the server verifies the client is legit
+	// tell the client they authenticated successfully
 	Server.send(client, "joined", null);
 
+	// create a new player, assign it to the client
+	// this will last for the duration of the connection
 	var player = Player.new(++playerId, "Anon"+playerId);
 	client.player = player;
+
+	// send the new client the chat history
 	for(key in chatHistory) {
 		Server.send(client, "message", chatHistory[key].name+": "+chatHistory[key].message);
 	}
+
+	// tell everyone the new client has joined
 	Server.broadcast("message", "Server: "+player.name+" has joined!");
+
+	// push the join to the chat history
 	pushChatHistory({name: "Server", message: player.name+" has joined!"});
+
+	// send an off-the-record PM to new player, thanking him for joining
 	Server.send(client, "message", "<i>&lt;Server: Thanks for joining!&gt;</i>");
+
+	// send the client his "self" player identity
 	Server.send(client, "Player", player);
 
+	// update everyone on the fresh list of players connected
 	var list = [];
 	for(key in Server.clients) {
 		list.push(Server.clients[key].player);
@@ -37,9 +63,13 @@ Server.onConnect = function(client) {
 
 Server.onDisconnect = function(client) {
 	var player = client.player;
+	// when the client leaves, tell everyone they left
 	Server.broadcast("message", "Server: "+player.name+" has left!");
+
+	// push the leave message to the chat
 	pushChatHistory({name: "Server", message: player.name+" has left!"});
 
+	// update everyone with a fresh player list
 	var list = [];
 	for(key in Server.clients) {
 		list.push(Server.clients[key].player);
@@ -48,12 +78,18 @@ Server.onDisconnect = function(client) {
 }
 
 Server.onMessage = function(client, key, payload) {
+	// if the client sent a message:
 	if(key == "message") {
+		// push it to history
 		pushChatHistory({name: client.player.name, message: payload});
+		// broadcast it to everyone
 		Server.broadcast("message", client.player.name+": "+payload);
 	} else if(key == "Player" && client.player.id == payload.id) {
+		// if the client sent a Player whose id matches theirs
 		client.player.name = payload.name;
+		// go ahead and update the client's player name
 
+		// let everyone know the new list with newly updated player name
 		var list = [];
 		for(key in Server.clients) {
 			list.push(Server.clients[key].player);
@@ -63,5 +99,6 @@ Server.onMessage = function(client, key, payload) {
 }
 
 Server.onError = function(client, error) {
+	// just log it lol
 	console.log("error! "+error);
 }
--- custom/GameServer.js
+++ custom/GameServer.js
@@ -1,28 +1,39 @@
+// require Server.js, create with null name, "Game" type, ignore the rest of the params, use default
 var Server = require('../Server.js')(null, "Game");
 
+// require Player.js on the server side, this smart object is passed back and forth between client and server
 var Player = require('../html/game/Player.js');
 
-var players = [];
+// keep iterating player count as players join
 var playerId = 0;
 
 Server.onConnect = function(client) {
+	// when the client connects AND authenticates, send the client a "joined message"
+	// this is arbitrary, we use this as client to verify the authentication went through
 	Server.send(client, "joined", null);
 
+	// create a new player and assign it to the client, this is persistent for the duration of the connection
 	var player = Player.new(++playerId, "Anon"+playerId, {x: 20, y: 20}, "red");
 	client.player = player;
 	
+	// send the client itself so it knows its id, name
 	Server.send(client, "Player", player);
 
+	// generate a list of players for each client
 	var list = [];
 	for(key in Server.clients) {
 		list.push(Server.clients[key].player);
 	}
+
+	// send everyone the player list, with each player's position and color being up to date
 	Server.broadcast("PlayerList", list);
 }
 
 Server.onDisconnect = function(client) {
-	var player = client.player;
 	
+	// update everyone with a new list of players, the client has already been removed and won't be updated
+	// but if we wanted the player which was removed, we could grab it with client.player one last time
+	// say, for a death animation or something, idk
 	var list = [];
 	for(key in Server.clients) {
 		list.push(Server.clients[key].player);
@@ -31,11 +42,14 @@ Server.onDisconnect = function(client) {
 }
 
 Server.onMessage = function(client, key, payload) {
+	// when we receive a message from a client, if it's a player whose ID matches the client's player ID:
 	if(key == "Player" && client.player.id == payload.id) {
+		// the client has sent us a player, it wants to update the player's name, pos, and color if changed
 		client.player.name = payload.name;
 		client.player.pos = payload.pos;
 		client.player.color = payload.color;
 
+		// broadcast this update to all clients with a fresh PlayerList
 		var list = [];
 		for(key in Server.clients) {
 			list.push(Server.clients[key].player);
@@ -45,5 +59,6 @@ Server.onMessage = function(client, key, payload) {
 }
 
 Server.onError = function(client, error) {
+	// just console log it
 	console.log("error! "+error);
 }
