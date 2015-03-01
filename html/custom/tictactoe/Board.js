e8f912048ae7709cd94def61d7fb540b0c10aded
Site Mao <site.mao1992@gmail.com>
2015-03-01 03:02:13 -0800

    skeleton code for board

---
+++ html/custom/tictactoe/Board.js
@@ -0,0 +1,46 @@
+(function(exports){
+    var Protocol = this['Protocol'];
+    if(Protocol === undefined) {
+        Protocol = require('../../server/Protocol.js');
+    }
+    var Player = this['Player'];
+    if(Player === undefined) {
+        Player = require('./Player.js');
+    }
+
+    exports.new = function(turn, teams, data, gamePlayers) {
+        var board = {
+            turn: turn,
+            teams: teams,
+            data: data,
+            gamePlayers: gamePlayers,
+            init: function() {
+            },
+            applyCommand: function(command) {
+            },
+            winner: function() {
+            }
+        }
+        return board;
+    }
+    exports.copy = function(b) {
+        if(!b) {
+            return null;
+        }
+        var gp = [];
+        for(key in b.gamePlayers) {
+            gp.push(Player.copy(b.gamePlayers[key]));
+        }
+        return exports.new(b.turn, b.teams, b.data, gp);
+    }
+    exports.import = function(json) {
+        return exports.copy(JSON.parse(json));
+    }
+
+    // **** Protocol Registration ****
+
+    Protocol.register("Board", function(payload) {
+        return exports.copy(payload);
+    });
+
+})(typeof exports === 'undefined'? this['Board']={}: exports);
